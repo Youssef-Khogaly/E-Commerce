4b62ba4611036849918b79949584e9f640cf68d5
Youssef-Khogaly <yossefibrahem8@gmail.com>
2026-07-06 11:24:20 +0300

    refactor checkout service

--- soruce_code/src/main/java/com/ecommerce/services/checkout/CheckoutService.java
+++ soruce_code/src/main/java/com/ecommerce/services/checkout/CheckoutService.java
@@ -9,13 +9,17 @@ import com.ecommerce.entities.Carts.CartItem;
 import com.ecommerce.entities.orders.Order;
 import com.ecommerce.entities.orders.OrderItem;
 import com.ecommerce.entities.orders.OrderState;
+import com.ecommerce.repository.Order.OrderJpaRepo;
 import com.ecommerce.services.StockService.StockService;
 import com.ecommerce.services.interfaces.CartService;
 import com.ecommerce.services.interfaces.IPaymentGatewayService;
 import com.ecommerce.services.interfaces.OrderService;
+import jakarta.persistence.EntityManager;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.time.Duration;
 import java.util.*;
@@ -23,7 +27,6 @@ import java.util.stream.Collectors;
 
 @Service
 @Slf4j
-@AllArgsConstructor
 public class CheckoutService {
 
     private final StockService stockService;
@@ -31,7 +34,21 @@ public class CheckoutService {
     private final CartService cartService;
     private final OrderService orderService;
     private final OrderMapper orderMapper;
-    private List<PaymentGatewayLineItem> toItemModel(List<OrderItem> orderItems,Map<Long , List<String>>productId_Images){
+    private final OrderJpaRepo orderJpaRepo;
+    private final TransactionTemplate orderCheckOutTransactionTemplate;
+
+    public CheckoutService(StockService stockService, IPaymentGatewayService paymentGatewayService, CartService cartService, OrderService orderService, OrderMapper orderMapper, OrderJpaRepo orderJpaRepo, TransactionTemplate transactionTemplate) {
+        this.stockService = stockService;
+        this.paymentGatewayService = paymentGatewayService;
+        this.cartService = cartService;
+        this.orderService = orderService;
+        this.orderMapper = orderMapper;
+        this.orderJpaRepo = orderJpaRepo;
+        this.orderCheckOutTransactionTemplate = transactionTemplate;
+        transactionTemplate.setIsolationLevel(TransactionDefinition.ISOLATION_READ_COMMITTED);
+    }
+
+    private List<PaymentGatewayLineItem> toItemModel(List<OrderItem> orderItems, Map<Long , List<String>>productId_Images){
 
         return orderItems.stream().map(p ->{
            var item = new PaymentGatewayLineItem();
@@ -54,6 +71,9 @@ public class CheckoutService {
         paymentOrder.setOrder_id(order.getId());
         return  paymentOrder;
     }
+
+
+
     public PaymentSession checkout(CheckoutReq req , Long cust_id)  {
 
         Cart cart = cartService.getCartForCheckout(cust_id).orElseThrow(() -> new BadRequestException("Customer id doesn't exists"));
@@ -63,35 +83,43 @@ public class CheckoutService {
                 , i -> i.getProduct().getImagesList().stream().map(pi -> pi.getImage().getImageUrl()).toList()));
 
         final Map<Long,Integer>id_quantityMap =  cart.getCartItemSet().stream().collect(Collectors.toUnmodifiableMap(i -> i.getProduct().getId() , CartItem::getQuantity));
-        try{
-            stockService.updatestock(id_quantityMap, StockService.StockOperation.RESERVE);
-        } catch (RuntimeException e) {
-            throw new RuntimeException(e);
-        }
-        Order order = null;
-        PaymentSession paymentSession = null;
-        try{
-            order = orderMapper.from(cart,req.shipping());
+
+            Order order = orderMapper.from(cart,req.shipping());
+            PaymentSession paymentSession = null;
             order.setState(OrderState.PENDING);
             order.setPaymentMethod(req.paymentMethod());
-            order = orderService.createOrder(order);
 
-            // create model
-            PaymentGatewayOrderModel paymentOrder = createPaymentOrderModel(order,productId_Images);
 
-            SessionGenerationCommand command =  new SessionGenerationCommand(paymentOrder, Duration.ofMinutes(40),"https://www.google.com/","https://www.google.com/" ,order.getPaymentMethod());
+            final Order finalOrder = order;
+            order =  orderCheckOutTransactionTemplate.execute((status) ->{
+                stockService.updatestock(id_quantityMap, StockService.StockOperation.RESERVE);
+                return  orderService.createOrder(finalOrder);
+            });
 
-            paymentSession = paymentGatewayService.generateSessionUrl(command);
-            order.setSession_id(paymentSession.getSession_id());
-            order.setExpireAt(paymentSession.getExpireAt().toEpochMilli());
-            return paymentSession;
-        }
-        catch (Exception e) {
-            stockService.updatestock(id_quantityMap, StockService.StockOperation.RELEASE);
-            if(order != null)
+
+
+
+            try{
+                // create model
+                PaymentGatewayOrderModel paymentOrder = createPaymentOrderModel(order,productId_Images);
+
+                SessionGenerationCommand command =  new SessionGenerationCommand(paymentOrder, Duration.ofMinutes(40),"https://www.google.com/","https://www.google.com/" ,order.getPaymentMethod());
+
+                paymentSession = paymentGatewayService.generateSessionUrl(command);
+                order.setSession_id(paymentSession.getSession_id());
+                order.setExpireAt(paymentSession.getExpireAt().toEpochMilli());
+                orderJpaRepo.save(order);
+            }catch (Exception e)
+            {
+                // session creation failed!!
+                stockService.updatestock(id_quantityMap, StockService.StockOperation.RELEASE);
                 orderService.deleteOrder(order.getId());
-            throw e;
-        }
+                throw new RuntimeException(e.getCause());
+            }
+
+
+            return paymentSession;
+
 
     }
 
