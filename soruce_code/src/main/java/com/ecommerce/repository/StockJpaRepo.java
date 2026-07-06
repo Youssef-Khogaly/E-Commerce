25254bce57b301d3b58b7da44000d7deb31eed46
Youssef-Khogaly <yossefibrahem8@gmail.com>
2026-07-06 10:16:55 +0300

    stock service improvement,

--- soruce_code/src/main/java/com/ecommerce/repository/StockJpaRepo.java
+++ soruce_code/src/main/java/com/ecommerce/repository/StockJpaRepo.java
@@ -6,13 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface StockJpaRepo extends JpaRepository<ProductStock,Long> {
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select s from ProductStock s where s.product.id in :ids")
-    List<ProductStock> findAllByIdForUpdate(List<Long> ids);
+    List<ProductStock> findAllByIdForUpdate(Collection<Long> ids);
 
 
 
