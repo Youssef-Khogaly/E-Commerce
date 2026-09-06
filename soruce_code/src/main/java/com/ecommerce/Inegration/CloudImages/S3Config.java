da216cb6fe2f5fdc02add584615e9fb7ad20f1c4
Youssef-Khogaly <yossefibrahem8@gmail.com>
2026-09-07 02:10:04 +0300

    override amazon s3 endpoint to point to localstack

--- soruce_code/src/main/java/com/ecommerce/Inegration/CloudImages/S3Config.java
+++ soruce_code/src/main/java/com/ecommerce/Inegration/CloudImages/S3Config.java
@@ -9,23 +9,29 @@ import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 @Configuration
 public class S3Config {
 
     @Bean
     S3Client s3Client(@Value("${AWS_SECRET_ACCESS_KEY}") String secKey
-            , @Value("${AWS_ACCESS_KEY_ID}") String accessKey , @Value("${AWS_REGION}") String region){
+            , @Value("${AWS_ACCESS_KEY_ID}") String accessKey , @Value("${AWS_REGION}") String region) throws URISyntaxException {
         var credentials = AwsBasicCredentials.create(accessKey,secKey);
         return S3Client
-                .builder().region(Region.of(region)).credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .builder().region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .endpointOverride(new URI("http://s3.localhost.localstack.cloud:4566"))
                 .build();
     }
     @Bean
     S3Presigner s3Presigner(@Value("${AWS_SECRET_ACCESS_KEY}") String secKey
-            , @Value("${AWS_ACCESS_KEY_ID}") String accessKey , @Value("${AWS_REGION}") String region){
+            , @Value("${AWS_ACCESS_KEY_ID}") String accessKey , @Value("${AWS_REGION}") String region) throws URISyntaxException {
         var credentials = AwsBasicCredentials.create(accessKey,secKey);
         return S3Presigner
                 .builder().region(Region.of(region)).credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .endpointOverride(new URI("http://s3.localhost.localstack.cloud:4566"))
                 .build();
     }
 }
